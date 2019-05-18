467a3a3d4fc68ed46858886221086f9e396b39d5
yhkim <kvoyant@gmail.com>
2019-05-18 11:26:20 +0900

    홈 프래그먼트 생성 - 수정중

---
+++ app/src/main/java/com/tjeit/tablayoutpractice/fragments/HomeFragment.java
@@ -0,0 +1,33 @@
+package com.tjeit.tablayoutpractice.fragments;
+
+import android.databinding.DataBindingUtil;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.tjeit.tablayoutpractice.R;
+import com.tjeit.tablayoutpractice.databinding.FragmentHomeBinding;
+
+
+public class HomeFragment extends Fragment {
+
+    FragmentHomeBinding binding;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_home)
+        return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        binding.welcomeMsgTxt.setText("홈 화면에 오신걸 환영합니다.");
+    }
+}
