af2651539720cd795b576a9afbd4bfaf15081ba5
yhkim <kvoyant@gmail.com>
2019-05-18 10:25:45 +0900

    탭 레이아웃 + 뷰페이저 + 프래그먼트 사용 예시

---
+++ app/src/main/java/com/tjeit/tablayoutpractice/BaseActivity.java
@@ -0,0 +1,13 @@
+package com.tjeit.tablayoutpractice;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+
+public abstract class BaseActivity extends AppCompatActivity {
+
+    public Context mContext = this;
+
+    public abstract void setupEvents();
+    public abstract void setValues();
+    public abstract void bindViews();
+}
--- app/src/main/java/com/tjeit/tablayoutpractice/MainActivity.java
+++ app/src/main/java/com/tjeit/tablayoutpractice/MainActivity.java
@@ -1,13 +1,36 @@
 package com.tjeit.tablayoutpractice;
 
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.tjeit.tablayoutpractice.databinding.ActivityMainBinding;
+
+public class MainActivity extends BaseActivity {
+
+    ActivityMainBinding act;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        bindViews();
+        setupEvents();
+        setValues();
+    }
+
+    @Override
+    public void setupEvents() {
+
+    }
+
+    @Override
+    public void setValues() {
+
+    }
+
+    @Override
+    public void bindViews() {
+        act = DataBindingUtil.setContentView(this, R.layout.activity_main);
     }
 }
