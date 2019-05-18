8783e15b9e71d58d7ecf86bff757447688c6c2f7
yhkim <kvoyant@gmail.com>
2019-05-18 11:19:03 +0900

    커스텀 탭뷰 마무리

--- app/src/main/java/com/tjeit/tablayoutpractice/MainActivity.java
+++ app/src/main/java/com/tjeit/tablayoutpractice/MainActivity.java
@@ -3,6 +3,9 @@ package com.tjeit.tablayoutpractice;
 import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.TextView;
 
 import com.tjeit.tablayoutpractice.databinding.ActivityMainBinding;
 
@@ -26,17 +29,22 @@ public class MainActivity extends BaseActivity {
         act.tabLayout.addTab(act.tabLayout.newTab().setText("검색"));
         act.tabLayout.addTab(act.tabLayout.newTab().setText("더보기"));
         //tabMode : fixed => scrollable
-//        act.tabLayout.addTab(act.tabLayout.newTab().setText("더보기"));
-//        act.tabLayout.addTab(act.tabLayout.newTab().setText("더보기"));
-//        act.tabLayout.addTab(act.tabLayout.newTab().setText("더보기"));
-//        act.tabLayout.addTab(act.tabLayout.newTab().setText("더보기"));
-//        act.tabLayout.addTab(act.tabLayout.newTab().setText("더보기"));
-//        act.tabLayout.addTab(act.tabLayout.newTab().setText("더보기"));
+        act.tabLayout.addTab(act.tabLayout.newTab().setCustomView(createCustomTabView("커스텀")));
+        act.tabLayout.addTab(act.tabLayout.newTab().setCustomView(createCustomTabView("커스텀2")));
     }
 
     @Override
     public void setValues() {
 
+    }
+
+    View createCustomTabView(String tabName){
+        View tabView = LayoutInflater.from(mContext).inflate(R.layout.custom_tab, null);
+
+        TextView nameTxt = tabView.findViewById(R.id.nameTxt);
+        nameTxt.setText(tabName);
+
+        return tabView;
     }
 
     @Override
